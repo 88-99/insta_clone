c604e93e0daf7b953315bcc1e9e5ed81b4da6794
KW <kw@KWnoMacBook-Pro.local>
2021-10-03 22:48:09 +0900

    BlogsControllerの PostMailer.post_mail(@blog).deliver をコメントアウト

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -17,7 +17,7 @@ class BlogsController < ApplicationController
       render :new
     else
       if @blog.save
-        PostMailer.post_mail(@blog).deliver
+        # PostMailer.post_mail(@blog).deliver
         redirect_to blogs_path, notice: "投稿しました！"
       else
         render :new
