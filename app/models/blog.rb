e6ad89f5b8f182131f7ec248f6ee19803c26de41
KW <kw@KWnoMacBook-Pro.local>
2021-10-04 23:50:46 +0900

    画像サイズを修正, imageとprofile_pictureにvalidationのpresence: trueを実装

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -3,5 +3,6 @@ class Blog < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :favorite_users, through: :favorites, source: :user
   validates :content, presence: true
+  validates :image, presence: true
   mount_uploader :image, ImageUploader
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   has_many :blogs
   has_many :favorites, dependent: :destroy
   before_validation { email.downcase! }
+  validates :profile_picture, presence: true
   validates :name, presence: true, length: { maximum: 30 }
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
